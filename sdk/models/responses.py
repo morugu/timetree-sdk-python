0b2414ffb80adc3ff55e85059c231efb53f33019
shoya shiraki <12c1055@gmail.com>
2020-03-05 21:31:58 +0900

    add calender responses

--- sdk/models/responses.py
+++ sdk/models/responses.py
@@ -19,3 +19,53 @@ class AttributesResponse(Base):
         self.name = name
         self.description = description
         self.image_url = image_url
+
+
+class CalenderResponse(Base):
+    def __init__(self, id=None, type=None, attributes=None, relationships=None, members=None):
+
+        super(CalenderResponse, self).__init__()
+
+        self.id = id
+        self.type = type
+        self.attributes = self.get_or_new_from_json_dict(attributes, CalenderAttributesResponse)
+
+
+class CalenderAttributesResponse(Base):
+    def __init__(self, name=None, created_at=None, description=None, image_url=None, color=None, order=None):
+
+        super(CalenderAttributesResponse, self).__init__()
+
+        self.name = name
+        self.created_at = created_at
+        self.description = description
+        self.image_url = image_url
+        self.color = color
+        self.order = order
+
+
+class RelationshipsResponse(Base):
+    def __init__(self, labels, members):
+
+        super(RelationshipsResponse, self).__init__()
+
+        self.labels = [self.get_or_new_from_json_dict(it, LabelResponse) for it in labels]
+        self.members = [self.get_or_new_from_json_dict(it, MemberResponse) for it in members]
+
+
+class LabelResponse(Base):
+    def __init__(self, id=None, type=None):
+
+        super(LabelResponse, self).__init__()
+
+        self.id = id
+        self.type = type
+
+
+class MemberResponse(Base):
+    def __init__(self, id=None, type=None):
+
+        super(MemberResponse, self).__init__()
+
+        self.id = id
+        self.type = type
