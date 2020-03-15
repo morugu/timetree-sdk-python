df82746ce103f37063d18bbdf7ce1a25b4e58ea2
shoya shiraki <12c1055@gmail.com>
2020-03-15 11:47:50 +0900

    add events model

--- timetree_sdk/models/base.py
+++ timetree_sdk/models/base.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Base(object):
 
     def __init__(self, **kwargs):
@@ -14,3 +17,24 @@ class Base(object):
         elif isinstance(data, dict):
             return cls.new_from_json_dict(data)
         return None
+
+    def as_json_string(self):
+        return json.dumps(self.as_json_dict(), sort_keys=True)
+
+    def as_json_dict(self):
+        data = {}
+        for key, value in self.__dict__.items():
+            if isinstance(value, (list, tuple, set)):
+                data[key] = list()
+                for item in value:
+                    if hasattr(item, 'as_json_dict'):
+                        data[key].append(item.as_json_dict())
+                    else:
+                        data[key].append(item)
+
+            elif hasattr(value, 'as_json_dict'):
+                data[key] = value.as_json_dict()
+            elif value is not None:
+                data[key] = value
+
+        return data
